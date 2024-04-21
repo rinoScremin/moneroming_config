58f597c5317e170fad2c145f57e94560313df13f
rinoScremin <scremin49@gmail.com>
2024-04-21 01:00:42 -0400

    Update create_TC64_USB.sh

--- create_TC64_USB.sh
+++ create_TC64_USB.sh
@@ -14,7 +14,7 @@ tce-load -wi gdisk
 wget http://tinycorelinux.net/15.x/x86_64/release/CorePure64-current.iso || { echo "Download failed"; exit 1; }
 
 # Automatically configure disk partitioning
-echo -e "o\nY\nn\n\n\n\n\nEF00\nw\nY\n" | sudo gdisk /dev/sdb
+echo -e "o\nY\nn\n\n\n\nEF00\nw\nY\n" | sudo gdisk /dev/sdb
 
 # Format the new partition (assuming it is /dev/sdb1)
 sudo mkfs.fat -F 32 -I /dev/sdb
@@ -33,42 +33,6 @@ sudo mount -o loop CorePure64-current.iso /mnt/iso
 # Copy necessary files to the USB drive
 sudo cp /mnt/iso/boot/vmlinuz64 /mnt/usb/boot/
 sudo cp /mnt/iso/boot/corepure64.gz /mnt/usb/boot/
-
-# Create and write GRUB configuration
-nano <<EOF | sudo tee /mnt/usb/boot/grub/grub.cfg
-
-set default=0
-set timeout=10
-set max_hd=10 # Maximum number of drives to check
-
-# Function to find and set root to the drive with /boot directory
-function find_and_set_root {
-    for i in `seq 0 10`; do
-        # Check if the drive exists by trying to list its contents
-        if [ -e (hd${i}) ]; then
-            echo "Checking (hd${i})..."
-            # Check if /boot directory exists on this drive
-            if [ -d (hd${i})/boot ]; then
-                set root=(hd${i})
-                echo "Found /boot on (hd${i})"
-                break
-            fi
-        fi
-    done
-}
-
-menuentry "Tiny Core64! XMRig" {
-    find_and_set_root
-    if [ "${root}" ]; then
-        echo "Booting from ${root}..."
-        linux /boot/vmlinuz64 quiet waitusb=5 restore=sdb/tce/mydata.tgz hugepagesz=1G hugepages=6 default_hugepagesz=1G restore=sdb/tce/mydata.tgz
-        initrd /boot/corepure64.gz
-    else
-        echo "Failed to find /boot directory on any drives."
-    fi
-}
-
-EOF
 
 # Unmount the USB drive and ISO
 sudo umount /mnt/usb

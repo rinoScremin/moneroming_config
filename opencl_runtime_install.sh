08c9e3e42f11a76d15c0c38eaabe8adc700b52c8
rinoScremin <scremin49@gmail.com>
2024-04-22 01:00:02 -0400

    Update opencl_runtime_install.sh

--- opencl_runtime_install.sh
+++ opencl_runtime_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-# Installing git-lfs 
 
 # make sure firmware is installed 
 tce-load -wi firmware-amdgpu.tcz
 tce-load -wi firmware-amd-ucode.tcz
 tce-load -wi xf86-video-amdgpu.tcz
 
+# Installing git-lfs 
 #download git-lfs files 
 cd ~
 curl -L -o git-lfs.tar.gz https://github.com/git-lfs/git-lfs/releases/download/v3.5.1/git-lfs-linux-amd64-v3.5.1.tar.gz

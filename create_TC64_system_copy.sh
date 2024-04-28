d0e8da8f01f92ed5f9f2413d35ec34060af65656
rinoScremin <scremin49@gmail.com>
2024-04-27 22:53:49 -0400

    Update create_TC64_system_copy.sh

--- create_TC64_system_copy.sh
+++ create_TC64_system_copy.sh
@@ -19,23 +19,17 @@ echo -e "o\nY\nn\n\n\n\nEF00\nw\nY\n" | sudo gdisk $1
 # Format the new partition (assuming it is /dev/sdb1)
 sudo mkfs.fat -F 32 -I $1
 
-# Mount the USB drive
-sudo mkdir -p /mnt/usb
-sudo mount $1 /mnt/usb
-
 # Install GRUB
-sudo grub-install --target=x86_64-efi --efi-directory=/mnt/usb --boot-directory=/mnt/usb/boot --removable
+sudo grub-install --target=x86_64-efi --efi-directory=$1 --boot-directory=$1/boot --removable
 
 #sudo mkdir -p /mnt/iso
 #sudo mount -o loop CorePure64-current.iso /mnt/iso
 # Copy necessary files to the USB drive
-sudo cp /mnt/sdb/boot/vmlinuz64 /mnt/usb/boot/
-sudo cp /mnt/sdb/boot/corepure64.gz /mnt/usb/boot/
-sudo cp /mnt/sdb/boot/grub/grub.cfg /mnt/usb/boot/grub/
-sudo cp -r /mnt/sdb/tce /mnt/usb
+sudo cp /mnt/sdb/boot/vmlinuz64 $1/boot/
+sudo cp /mnt/sdb/boot/corepure64.gz $1/boot/
+sudo cp /mnt/sdb/boot/grub/grub.cfg $1/boot/grub/
+sudo cp -r /mnt/sdb/tce $1
 # Unmount the USB drive and ISO
-sudo umount /mnt/usb
-#sudo umount /mnt/iso
 # Complete setup
 echo "Tiny Core Linux is ready on the USB drive!"
 
